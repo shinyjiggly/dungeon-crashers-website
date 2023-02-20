6745578b43a24d9f6ddfe22143e72cc73eb8f374
shinyjiggly <agenttat@gmail.com>
2023-02-20 02:16:10 -0800

    Pizza Party
    -added a left beef landing page
    -removed the list from the side menu
    -added a zip file of Up On The Housetop
    -added a directory for secret stuff...?
    
    To-do:
    -work on the "meet the dungeon crashers" page
    --particularly the stylesheet stuff
    --and then import everyone with that style
    -figure out how to order the locations
    --add those locations!
    -maybe figure out how to make the side menu work from any amount of directories inward?

--- websitestuffs/addstuff.js
+++ websitestuffs/addstuff.js
@@ -1,45 +1,45 @@
 //menuarea
 let menuareaHTML = '<h2>Links</h2>' +
-'<ul><li><a href="' + 'index.html">Main page</a></li>' +
-'<li><a href="' + 'dungeon.html">What is a voidco dungeon?</a></li>' + 
-'<li><a href="' + 'crashers.html">Meet the Dungeon Crashers </a> <img src="construct.png" title="Under Construction!" /></li>' +
-'<li><a href="' + 'locations.html">Locations of Interest</a> <img src="construct.png" title="Under Construction!" /></li>' +
-'<li><a href="' + 'pokemons.html">Starter Online Pokedex</a></li>' +
-'<li><a href="' + 'missions.html">Unfinished Missions</a></li>' +
-'<li><a href="' + 'https://mspfa.com/?s=38989&p=1">None Pizza With Left Beef</a></li>' +
-'<li><a href="' + 'https://charcherry-weekly.tumblr.com/">Charcherry Weekly</a></li>' +
-'<li><a href="' + 'friend.html">look at my pokemon</a></li></ul>' +
+'<a href="' + 'index.html">Main page</a><br>' +
+'<a href="' + 'dungeon.html">What is a voidco dungeon?</a><br>' + 
+'<a href="' + 'crashers.html">Meet the Dungeon Crashers </a> <img src="construct.png" title="Under Construction!" /><br>' +
+'<a href="' + 'locations.html">Locations of Interest</a> <img src="construct.png" title="Under Construction!" /><br>' +
+'<a href="' + 'pokemons.html">Starter Online Pokedex</a><br>' +
+'<a href="' + 'missions.html">Unfinished Missions</a><br>' +
+'<a href="' + 'leftbeef.html">None Pizza With Left Beef</a><br>' +
+'<a href="' + 'https://charcherry-weekly.tumblr.com/">Charcherry Weekly</a><br>' +
+'<a href="' + 'friend.html">look at my pokemon</a><br>' +
 '<hr><hr>' +
-'<h2>The Duct-tape Zone</h2><ul> <li><a href="' + 'ooc_dtape.html">...Duct tape?</a></li>' + 
-'<li><a href="' + 'ooc_pokencounter.html">Pokemon Encounter Generator</a></li>' +
-'<li><a href="' + 'ooc_systems.html">Systems used for combat and such</a></li>' +
-'<li><a href="' + 'https://letssosl.boards.net/thread/274/pokemon-world-mage-quest">Mage Quest</a></li>' +
-'<li><a href="' + 'http://jingloria.wertercatt.com/oldsite/pichuinfo.htm">Pichu Quest (pre-scratch Mage Quest)</a></li>' +
-'<li><a href="' + 'starterclock.html">Current Time</a></li>' +
-'<li><a href="' + 'https://discord.gg/Ykh6hrf">Join our discord server!</a></li>' +
-'<li><a href="' + 'ooc_logs.html">Logs</a></li>' +
-'<li><a href="' + 'http://jingloria.wertercatt.com/lostpawns/">Lost Pawns Website</a></li>' + 
-'<li><a href="' + 'http://www.ifelse95.xyz/index.html">ifelse95\'s Website</a></li>' + 
-'<li><a href="' + 'http://wertercatt.com/">wetercatt\'s Website</a></li>';
+'<h2>The Duct-tape Zone</h2><a href="' + 'ooc_dtape.html">...Duct tape?</a><br>' + 
+'<a href="' + 'ooc_pokencounter.html">Pokemon Encounter Generator</a><br>' +
+'<a href="' + 'ooc_systems.html">Systems used for combat and such</a><br>' +
+'<a href="' + 'https://letssosl.boards.net/">Let\'s SOSL Forums</a><br>' +
+'<a href="' + 'http://jingloria.wertercatt.com/oldsite/pichuinfo.htm">Pichu Quest (pre-scratch Mage Quest)</a><br>' +
+'<a href="' + 'starterclock.html">Current Time</a><br>' +
+'<a href="' + 'https://discord.gg/Ykh6hrf">Join our discord server!</a><br>' +
+'<a href="' + 'ooc_logs.html">Logs</a><br>' +
+'<a href="' + 'http://jingloria.wertercatt.com/lostpawns/">Lost Pawns Website</a><br>' + 
+'<a href="' + 'http://www.ifelse95.xyz/index.html">ifelse95\'s Website</a><br>' + 
+'<a href="' + 'http://wertercatt.com/">wetercatt\'s Website</a><br>';
 
 //icmenuarea
-let icmenuareaHTML = '<h2>Links</h2>' +
-'<ul><li><a href="' + 'index.html">Main page</a></li>' +
-'<li><a href="' + 'dungeon.html">What is a voidco dungeon?</a></li>' + 
-'<li><a href="' + 'crashers.html">Meet the Dungeon Crashers </a> <img src="construct.png" title="Under Construction!" /></li>' +
-'<li><a href="' + 'locations.html">Locations of Interest</a> <img src="construct.png" title="Under Construction!" /></li>' +
-'<li><a href="' + 'pokemons.html">Starter Online Pokedex</a></li>' +
-'<li><a href="' + 'missions.html">Unfinished Missions</a></li>' +
-'<li><a href="' + 'https://mspfa.com/?s=38989&p=1">None Pizza With Left Beef</a></li>' +
-'<li><a href="' + 'https://charcherry-weekly.tumblr.com/">Charcherry Weekly</a></li>' +
-'<li><a href="' + 'friend.html">look at my pokemon</a></li></ul>' +
+let icmenuareaHTML = '<h2>Links</h2><br>' +
+'<a href="' + 'index.html">Main page</a><br>' +
+'<a href="' + 'dungeon.html">What is a voidco dungeon?</a><br>' + 
+'<a href="' + 'crashers.html">Meet the Dungeon Crashers </a> <img src="construct.png" title="Under Construction!" /><br>' +
+'<a href="' + 'locations.html">Locations of Interest</a> <img src="construct.png" title="Under Construction!" /><br>' +
+'<a href="' + 'pokemons.html">Starter Online Pokedex</a><br>' +
+'<a href="' + 'missions.html">Unfinished Missions</a><br>' +
+'<a href="' + 'leftbeef.html">None Pizza With Left Beef</a><br>' +
+'<a href="' + 'https://charcherry-weekly.tumblr.com/">Charcherry Weekly</a><br>' +
+'<a href="' + 'friend.html">look at my pokemon</a><br>' +
 '<hr>' +
-'<h2>Other Links</h2><ul> <li><a href="' + 'http://jingloria.wertercatt.com/">Jingloria Website</a></li>' + 
-'<li><a href="' + 'desertia.robotics">Desertia Website</a></li>' +
-'<li><a href="' + 'spaceport-mall.shop">Spaceport Website</a></li>' ;
+'<h2>Other Links</h2><a href="' + 'http://jingloria.wertercatt.com/">Jingloria Website</a><br>' + 
+'<a href="' + 'desertia.robotics">Desertia Website</a><br>' +
+'<a href="' + 'spaceport-mall.shop">Spaceport Website</a><br>' ;
 
 //header
-let headerHTML = '<a href="index.html"><img src="sitelogo.png" alt="SBARG 2.5: Dungeon Crashers"></a>' ;
+let headerHTML = '<a href="index.html"><img src="https://jingloria.wertercatt.com/dungeoncrashers/sitelogo.png" alt="SBARG 2.5: Dungeon Crashers"></a>' ;
 
 //footer
 let footerHTML = '<br><hr><br><p>site made by shinyJiggly, photos from Unsplash ((special thanks to ifelse95 and wertercatt))</p>' ;
